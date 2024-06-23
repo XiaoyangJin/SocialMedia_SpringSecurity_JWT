9744f8b416e5f3db3759167a068fc2e0887036d5
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-06-22 22:58:42 -0400

    Init Post entity

---
+++ SocialMedia/src/main/java/com/springsecurity/SocialMedia/entity/Post.java
@@ -0,0 +1,16 @@
+package com.springsecurity.SocialMedia.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post {
+    private Long id;
+    private String title;
+    private String content;
+}
