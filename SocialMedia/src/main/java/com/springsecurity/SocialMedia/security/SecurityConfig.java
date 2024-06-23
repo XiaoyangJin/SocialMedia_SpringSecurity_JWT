805c2939873bc75f6a332b8460d8ef7b0c33619c
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-06-23 09:33:16 -0400

    config security

---
+++ SocialMedia/src/main/java/com/springsecurity/SocialMedia/security/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.springsecurity.SocialMedia.security;
+
+import com.springsecurity.SocialMedia.service.impl.MyUserDetailServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(MyUserDetailServiceImpl myUserDetailService, PasswordEncoder passwordEncoder){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
+        daoAuthenticationProvider.setUserDetailsService(myUserDetailService);
+        return daoAuthenticationProvider;
+    }
+}
